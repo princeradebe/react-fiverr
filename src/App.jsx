cc4ccea3a43d064d0de0b968317374fde4fb8e7c
Prince Radebe <princeradebe@gmail.com>
2023-02-22 21:43:36 +0200

    FV-3: Add NavBar links and color

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
 
   return (
     <div className="App">
-      Hello World
+      <Navbar />
     </div>
   )
 }
