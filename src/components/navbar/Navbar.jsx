463776b3eb93306cfc850f18b4bc9fcdae95f133
Prince Radebe <princeradebe@gmail.com>
2023-02-24 00:36:24 +0200

    FV-4: Add active state for navbar when it is active

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+// import { Link } from 'react-router-dom'
 import './Navbar.scss'
 
 const Navbar = () => {
-    const [active, setActive] = useState(false)
+    const [active, setActive] = useState(false);
+    const isActive = () => {
+        window.scrollY > 0 ? setActive(true) : setActive(false)
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', isActive);
+
+        return () => {
+            window.removeEventListener('scroll', isActive);
+        }
+    }, [])
+
     return (
-        <div className={active ? "navbar ctive" : "navbar"}>
+        <div className={active ? "navbar active" : "navbar"}>
             <div className="container">
                 <div className="logo">
                     {/* <Link to="/"> */}
@@ -23,11 +35,15 @@ const Navbar = () => {
                     <button>Join</button>
                 </div>
             </div>
-            <hr />
-            <div className="menu">
-                <span>Test</span>
-                <span>Test2</span>
-            </div>
+            {active &&
+                <>
+                    <hr />
+                    <div className="menu">
+                        <span>Test</span>
+                        <span>Test2</span>
+                    </div>
+                </>
+            }
         </div>
     )
 }
