6db7e25beecf1128ffdce197743dd18cdab47c28
Prince Radebe <princeradebe@gmail.com>
2023-02-24 00:06:08 +0200

    Set state for navlink

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.scss'
 
 const Navbar = () => {
+    const [active, setActive] = useState(false)
     return (
-        <div className={active ? "navbaractive" : "navebar"}>
+        <div className={active ? "navbar ctive" : "navbar"}>
             <div className="container">
                 <div className="logo">
                     {/* <Link to="/"> */}
