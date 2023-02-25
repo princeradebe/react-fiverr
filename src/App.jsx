43e703903ecac6b502c21232653fe460c7c675df
Prince Radebe <princeradebe@gmail.com>
2023-02-25 18:21:22 +0200

    Add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navbar />,
+    },
+  ]);
 
   return (
     <div className="App">
-      <Navbar />
+      <RouterProvider router={router} />
       <h1>Duis nostrud voluptate excepteur magna id amet dolore in sunt ad consectetur veniam fugiat cupidatat elit. Minim non enim culpa amet excepteur Lorem occaecat. Nisi velit mollit deserunt commodo ea aliqua duis excepteur in Lorem ullamco occaecat veniam. Nulla quis proident id mollit magna. Ullamco minim do reprehenderit adipisicing ad ipsum excepteur aute.</h1>
       <h1>Duis nostrud voluptate excepteur magna id amet dolore in sunt ad consectetur veniam fugiat cupidatat elit. Minim non enim culpa amet excepteur Lorem occaecat. Nisi velit mollit deserunt commodo ea aliqua duis excepteur in Lorem ullamco occaecat veniam. Nulla quis proident id mollit magna. Ullamco minim do reprehenderit adipisicing ad ipsum excepteur aute.</h1>
       <h1>Duis nostrud voluptate excepteur magna id amet dolore in sunt ad consectetur veniam fugiat cupidatat elit. Minim non enim culpa amet excepteur Lorem occaecat. Nisi velit mollit deserunt commodo ea aliqua duis excepteur in Lorem ullamco occaecat veniam. Nulla quis proident id mollit magna. Ullamco minim do reprehenderit adipisicing ad ipsum excepteur aute.</h1>
