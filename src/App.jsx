e8dac7a49f9fdd9d12648314fcb89239d7880193
Prince Radebe <princeradebe@gmail.com>
2023-02-25 23:28:05 +0200

    FV-6: Resolves myGigs spelling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+// import './App.css'
 import './App.scss'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
